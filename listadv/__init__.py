afa8aa716af113581c4dd404bbdd63fbfa925d4c
nyaboron <nyaboron>
2021-07-29 11:19:47 +0200

    css style, set style cache to 0 seconds in the app config

--- listadv/__init__.py
+++ listadv/__init__.py
@@ -8,6 +8,7 @@ def create_app(test_config=None):
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'listadv.sqlite'),
+        SEND_FILE_MAX_AGE_DEFAULT = 0
     )
 
     if test_config is None:
